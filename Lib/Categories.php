4bd2d9fb94ae80dd151d0fb015356bac4092cf4c
Midori Koçak <mtkocak@mtkocak.net>
2014-08-23 17:54:00 +0300

    added namespaces folder name change

--- Lib/Categories.php
+++ Lib/Categories.php
@@ -1,27 +1,31 @@
 <?php
 /**
-* Tüm sistemdeki dosyaları yönetecek olan dosya sınıfıdır.
+* Tüm sistemdeki kategorileri yönetecek olan kategori sınıfıdır.
 *
-* Sistemdeki dosyaların düzenlenmesini, silinmesini, görüntülenmesini, 
+* Sistemdeki kategorilerin düzenlenmesini, silinmesini, görüntülenmesini, 
 * listelenmesini ve eklenmesini kontrol eden sınıftır.
 *
 * @author     Midori Kocak <mtkocak@mtkocak.net>
 */
-class Files{
+
+namespace Midori\Cms;
+
+class Categories{
 	
     /**
-    * Dosyanın tekil id'sini tutan değişken. Başka dosyalarle karışmamasını sağlar
+    * Kategorinin tekil id'sini tutan değişken. Başka kategorilerle karışmamasını sağlar
     *
     * @var int
     */
     public $id;
  
     /**
-    * Dosya ismi
+    * Kategori başlığı
     *
     * @var string
     */
-    public $filename;
+    public $title;
+	
 	
     /**
     * Veritabanı bağlantısını tutacak olan değişken.
@@ -49,29 +53,30 @@ class Files{
             return $e->getMessage();
         }
     }
+    
  
     /**
-    * Dosya ekleyen metod, verilerin kaydedilmesini sağlar.
+    * Kategori ekleyen metod, verilerin kaydedilmesini sağlar.
     *
-    * @param string $title Dosya başlığı
-    * @param string $content Dosya içeriği
-    * @param int $category_id Dosya kategorisinin benzersiz kimliği
+    * @param string $title Kategori başlığı
+    * @param string $content Kategori içeriği
+    * @param int $category_id Kategori kategorisinin benzersiz kimliği
     * @return bool eklendiyse doğru, eklenemediyse yanlış değer döndürsün
     */
-    public function add($filename){
+    public function add($title){
 		
 		
         // Önce veritabanı sorgumuzu hazırlayalım.
-        $query = $this->db->prepare("INSERT INTO posts SET filename=:dosyaadi");
+        $query = $this->db->prepare("INSERT INTO categories SET title=:baslik");
 	
         $insert = $query->execute(array(
-            "dosyaadi"=>$filename,
+            "baslik"=>$title
         ));
 	
         if($insert){
             // Veritabanı işlemi başarılı ise sınıfın objesine ait değişkenleri değiştirelim
             $this->id = $this->db->lastInsertId();
-            $this->filename = $filename;
+            $this->title = $title;
             
             return true;
         }
@@ -81,27 +86,27 @@ class Files{
     }
 
     /**
-    * Tek bir dosyanın gösterilmesini sağlayan method
+    * Tek bir kategorinin gösterilmesini sağlayan method
     *
-    * @param int $id Dosyanın benzersiz index'i
+    * @param int $id Kategorinin benzersiz index'i
     * @return array gösterilebildyise dizi türünde verileri döndürsün, gösterilemediyse false, yanlış değeri döndürsün
     */
     public function view($id){
 
         // Eğer daha önceden sorgu işlemi yapıldıysa, sınıf objesine yazılmıştır.
         if($id == $this->id){
-            return array("id"=>$this->id,"filename"=>$this->filename);
+            return array("id"=>$this->id,"title"=>$this->title);
         }
         else{
             // Buradan anlıyoruz ki veri henüz çekilmemiş. Veriyi çekmeye başlayalım
-            $query = $this->db->prepare("SELECT * FROM files WHERE id=:id");
+            $query = $this->db->prepare("SELECT * FROM posts WHERE id=:id");
             $query->execute(array(':id'=>$id));
 		
             if($query){
                 $result = $query->fetch(PDO::FETCH_ASSOC);
                 
                 $this->id = $result['id'];
-                $this->title = $result['filename'];
+                $this->title = $result['title'];
                 
                 return $result;
             }
@@ -112,12 +117,12 @@ class Files{
     }
 	
     /**
-    * Tüm dosyaların listelenmesini sağlayan metod.
+    * Tüm kategorilerin listelenmesini sağlayan metod.
     *
     * @return bool listelenebildiyse doğru, listelenemediyse yanlış değer döndürsün
     */
     public function index(){
-		$query = $this->db->prepare("SELECT * FROM files");
+		$query = $this->db->prepare("SELECT * FROM categories");
         $query->execute();
         if($query){
             // Buradaki fetchAll metoduyla tüm değeleri diziye çektik.
@@ -131,22 +136,21 @@ class Files{
 
 
     /**
-    * Dosya düzenleyen metod. Verilen Id bilginse göre, alınan bilgi ile sistemdeki bilgiyi değiştiren
+    * Kategori düzenleyen metod. Verilen Id bilginse göre, alınan bilgi ile sistemdeki bilgiyi değiştiren
     * güncelleyen metod.
     *
-    * @param int $id Dosyanın benzersiz index'i
+    * @param int $id Kategorinin benzersiz index'i
     * @return bool düzenlendiyse doğru, eklenemediyse yanlış değer döndürsün
     */
-    public function edit($id, $filename){
+    public function edit($id, $title){
         
-        // Tarih içeren alanları elle girmiyoruz. Sistemden doğrudan isteyen fonksiyonumuz var.
-        $date = $this->getDate();
 		
         // Önce veritabanı sorgumuzu hazırlayalım.
-        $query = $this->db->prepare("UPDATE files SET filename=:filename WHERE id=:id");
+        $query = $this->db->prepare("UPDATE categories SET title=:baslik WHERE id=:id");
 	
         $update = $query->execute(array(
-            "filename"=>$filename,
+            "baslik"=>$title,
+            "id"=>$id
         ));
 		
         if ( $update ){
@@ -159,14 +163,14 @@ class Files{
     }
 
     /**
-    * Dosya silen metod, verilerin silinmesini sağlar.
+    * Kategori silen metod, verilerin silinmesini sağlar.
     * Geri dönüşü yoktur.
     *
-    * @param int $id Dosyanın benzersiz index'i
+    * @param int $id Kategorinin benzersiz index'i
     * @return bool silindiyse doğru, eklenemediyse yanlış değer döndürsün
     */
     public function delete($id){
-        $query = $this->db->prepare("DELETE FROM files WHERE id = :id");
+        $query = $this->db->prepare("DELETE FROM categories WHERE id = :id");
         $delete = $query->execute(array(
            'id' => $id
         ));
--- Lib/Files.php
+++ Lib/Files.php
@@ -1,28 +1,30 @@
 <?php
 /**
-* Tüm sistemdeki kategorileri yönetecek olan kategori sınıfıdır.
+* Tüm sistemdeki dosyaları yönetecek olan dosya sınıfıdır.
 *
-* Sistemdeki kategorilerin düzenlenmesini, silinmesini, görüntülenmesini, 
+* Sistemdeki dosyaların düzenlenmesini, silinmesini, görüntülenmesini, 
 * listelenmesini ve eklenmesini kontrol eden sınıftır.
 *
 * @author     Midori Kocak <mtkocak@mtkocak.net>
 */
-class Categories{
+
+namespace Midori\Cms;
+
+class Files{
 	
     /**
-    * Kategorinin tekil id'sini tutan değişken. Başka kategorilerle karışmamasını sağlar
+    * Dosyanın tekil id'sini tutan değişken. Başka dosyalarle karışmamasını sağlar
     *
     * @var int
     */
     public $id;
  
     /**
-    * Kategori başlığı
+    * Dosya ismi
     *
     * @var string
     */
-    public $title;
-	
+    public $filename;
 	
     /**
     * Veritabanı bağlantısını tutacak olan değişken.
@@ -50,30 +52,29 @@ class Categories{
             return $e->getMessage();
         }
     }
-    
  
     /**
-    * Kategori ekleyen metod, verilerin kaydedilmesini sağlar.
+    * Dosya ekleyen metod, verilerin kaydedilmesini sağlar.
     *
-    * @param string $title Kategori başlığı
-    * @param string $content Kategori içeriği
-    * @param int $category_id Kategori kategorisinin benzersiz kimliği
+    * @param string $title Dosya başlığı
+    * @param string $content Dosya içeriği
+    * @param int $category_id Dosya kategorisinin benzersiz kimliği
     * @return bool eklendiyse doğru, eklenemediyse yanlış değer döndürsün
     */
-    public function add($title){
+    public function add($filename){
 		
 		
         // Önce veritabanı sorgumuzu hazırlayalım.
-        $query = $this->db->prepare("INSERT INTO categories SET title=:baslik");
+        $query = $this->db->prepare("INSERT INTO posts SET filename=:dosyaadi");
 	
         $insert = $query->execute(array(
-            "baslik"=>$title
+            "dosyaadi"=>$filename,
         ));
 	
         if($insert){
             // Veritabanı işlemi başarılı ise sınıfın objesine ait değişkenleri değiştirelim
             $this->id = $this->db->lastInsertId();
-            $this->title = $title;
+            $this->filename = $filename;
             
             return true;
         }
@@ -83,27 +84,27 @@ class Categories{
     }
 
     /**
-    * Tek bir kategorinin gösterilmesini sağlayan method
+    * Tek bir dosyanın gösterilmesini sağlayan method
     *
-    * @param int $id Kategorinin benzersiz index'i
+    * @param int $id Dosyanın benzersiz index'i
     * @return array gösterilebildyise dizi türünde verileri döndürsün, gösterilemediyse false, yanlış değeri döndürsün
     */
     public function view($id){
 
         // Eğer daha önceden sorgu işlemi yapıldıysa, sınıf objesine yazılmıştır.
         if($id == $this->id){
-            return array("id"=>$this->id,"title"=>$this->title);
+            return array("id"=>$this->id,"filename"=>$this->filename);
         }
         else{
             // Buradan anlıyoruz ki veri henüz çekilmemiş. Veriyi çekmeye başlayalım
-            $query = $this->db->prepare("SELECT * FROM posts WHERE id=:id");
+            $query = $this->db->prepare("SELECT * FROM files WHERE id=:id");
             $query->execute(array(':id'=>$id));
 		
             if($query){
                 $result = $query->fetch(PDO::FETCH_ASSOC);
                 
                 $this->id = $result['id'];
-                $this->title = $result['title'];
+                $this->title = $result['filename'];
                 
                 return $result;
             }
@@ -114,12 +115,12 @@ class Categories{
     }
 	
     /**
-    * Tüm kategorilerin listelenmesini sağlayan metod.
+    * Tüm dosyaların listelenmesini sağlayan metod.
     *
     * @return bool listelenebildiyse doğru, listelenemediyse yanlış değer döndürsün
     */
     public function index(){
-		$query = $this->db->prepare("SELECT * FROM categories");
+		$query = $this->db->prepare("SELECT * FROM files");
         $query->execute();
         if($query){
             // Buradaki fetchAll metoduyla tüm değeleri diziye çektik.
@@ -133,21 +134,22 @@ class Categories{
 
 
     /**
-    * Kategori düzenleyen metod. Verilen Id bilginse göre, alınan bilgi ile sistemdeki bilgiyi değiştiren
+    * Dosya düzenleyen metod. Verilen Id bilginse göre, alınan bilgi ile sistemdeki bilgiyi değiştiren
     * güncelleyen metod.
     *
-    * @param int $id Kategorinin benzersiz index'i
+    * @param int $id Dosyanın benzersiz index'i
     * @return bool düzenlendiyse doğru, eklenemediyse yanlış değer döndürsün
     */
-    public function edit($id, $title){
+    public function edit($id, $filename){
         
+        // Tarih içeren alanları elle girmiyoruz. Sistemden doğrudan isteyen fonksiyonumuz var.
+        $date = $this->getDate();
 		
         // Önce veritabanı sorgumuzu hazırlayalım.
-        $query = $this->db->prepare("UPDATE categories SET title=:baslik WHERE id=:id");
+        $query = $this->db->prepare("UPDATE files SET filename=:filename WHERE id=:id");
 	
         $update = $query->execute(array(
-            "baslik"=>$title,
-            "id"=>$id
+            "filename"=>$filename,
         ));
 		
         if ( $update ){
@@ -160,14 +162,14 @@ class Categories{
     }
 
     /**
-    * Kategori silen metod, verilerin silinmesini sağlar.
+    * Dosya silen metod, verilerin silinmesini sağlar.
     * Geri dönüşü yoktur.
     *
-    * @param int $id Kategorinin benzersiz index'i
+    * @param int $id Dosyanın benzersiz index'i
     * @return bool silindiyse doğru, eklenemediyse yanlış değer döndürsün
     */
     public function delete($id){
-        $query = $this->db->prepare("DELETE FROM categories WHERE id = :id");
+        $query = $this->db->prepare("DELETE FROM files WHERE id = :id");
         $delete = $query->execute(array(
            'id' => $id
         ));
