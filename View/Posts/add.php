a60f21faaa6cd18922f1db958a1a6af956a568ef
Midori Koçak <mtkocak@mtkocak.net>
2014-08-25 15:18:32 +0300

    add edit view metodları

---
+++ View/Posts/add.php
@@ -0,0 +1,40 @@
+/*
+*
+* Adminlerin yeni yazı eklediği sayfa
+*
+* Veriyi kullanıcıdan alacak ve gerekli yerlere göndereceğiz.
+* Kategori seçeceğimiz için kategorilerin dizi olarak ($categories) 
+* dosyaya gelmiş olması gerekiyor. Çünkü bağlantılı bilgi.
+* Biz de bundan liste oluşturacağız.
+*
+* @author Midori Kocak 2014
+*
+*/
+
+<form action="add.php" method="post">
+  <div class="row">
+    <div class="large-12 columns">
+      <label>Başlık
+        <input type="text" placeholder="large-12.columns" />
+      </label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-12 columns">
+      <label>İçerik
+        <textarea placeholder="small-12.columns"></textarea>
+      </label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-12 columns">
+      <label>Kategoriler
+        <select>
+            <?php foreach ($categories as $category):?>
+                <option value="<?=$category?>"><?=$category?></option>
+            <?php endforeach:?>
+        </select>
+      </label>
+    </div>
+  </div>
+</form>
