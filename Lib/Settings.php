002bdafca500e87d4d49c5dc6a71f22956c0db8d
Midori Koçak <mtkocak@mtkocak.net>
2014-09-01 18:15:41 +0300

    Settings

---
+++ Lib/Settings.php
@@ -0,0 +1,212 @@
+<?php
+/**
+* Tüm sistemdeki ayarları yönetecek sınıftır.
+*
+* Sistemin kurulumunu yapan ve ayarları yöneten sınıf. Sadece tek bir ayar olmasına izin vermeli.
+*
+* @author     Midori Kocak <mtkocak@mtkocak.net>
+*/
+
+namespace Midori\Cms;
+
+use \PDO;
+
+class Settings extends Assets{
+	
+
+    /**
+    * Site Başlığı
+    *
+    * @var string
+    */
+    public $title;
+    
+    /**
+    * Site açıklaması
+    *
+    * @var string
+    */
+    public $description;
+    
+    /**
+    * Site altbilgisi
+    *
+    * @var string
+    */
+    public $copyright;
+ 
+    /**
+    * Sisteme ayar ekleyen metod. Bir nevi kurulum da denilebilir.
+    * Ancak önce sistemde ayar olup olmadığını kontrol etmeli, ayar varsa, hata vermelidir.
+    *
+    * @param string $title Site başlığı
+    * @param string $description Yönetici parola
+    * @param string $copyright Yönetici e-posta
+    * @return bool eklendiyse doğru, eklenemediyse yanlış değer döndürsün
+    */
+    public function add($title=null, $description=null, $copyright=null){
+
+        if(!$this->checkLogin()){
+            return false;
+        }
+
+        $settings = $this->view();
+        if(!empty($settings['setting']))
+        {
+
+            return  array('template'=>'admin','render'=>true,'setting'=>$settings['setting'], 'renderFile'=>'edit');
+        }
+
+        if($title!=null)
+        {
+            $settings = $this->show();
+
+            if(!empty($settings['settings'])){
+                return false;
+            }
+
+            // Önce veritabanı sorgumuzu hazırlayalım.
+            $query = $this->db->prepare("INSERT INTO settings SET title=:baslik, description=:aciklama, copyright=:altbilgi");
+
+            $insert = $query->execute(array(
+                "baslik"=>$title,
+                "aciklama"=>$description,
+                "altbilgi"=>$copyright
+            ));
+
+            if($insert){
+                // Veritabanı işlemi başarılı ise sınıfın objesine ait değişkenleri değiştirelim
+                $this->id = $this->db->lastInsertId();
+                $this->title = $title;
+                $this->description = $description;
+                $this->copyright = $copyright;
+
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return array('render'=>true,'template'=>'admin');
+        }
+    }
+
+    /**
+    * Tek bir kullanıcı verisini edit işlemine yani profil sayfasına gönderen metod. Render edilmesin
+    *
+    * @param int $id Kullanıcının benzersiz index'i
+    * @return array gösterilebildyise dizi türünde verileri döndürsün, gösterilemediyse false, yanlış değeri döndürsün
+    */
+    public function view($id=null){
+
+        // Eğer daha önceden sorgu işlemi yapıldıysa, sınıf objesine yazılmıştır.
+        if($id!=null && $id == $this->id){
+            return array("id"=>$this->id,"title"=>$this->title, "description"=>$this->description, "copyright"=>$this->copyright);
+        }
+        else{
+
+            // Buradan anlıyoruz ki veri henüz çekilmemiş. Veriyi çekmeye başlayalım
+            $query = $this->db->prepare("SELECT * FROM settings LIMIT 1");
+            $query->execute();
+            if($query){
+                $setting = $query->fetch(PDO::FETCH_ASSOC);
+                
+                $result = array('setting'=>$setting);
+                
+                $this->id = $setting['id'];
+                $this->title = $setting['title'];
+                $this->description = $setting['description'];
+                $this->copyright = $setting['copyright'];
+
+                $result =  array('template'=>'admin','render'=>true,'setting'=>$setting, 'renderFile'=>'edit');
+                return $result;
+            }
+        }
+	
+        // Eğer işlem başarısız olduysa, false, yanlış değer döndürelim.
+        return false;
+    }
+    
+    /**
+    * Tüm ayarların listelenmesini sağlayan metod.
+    *
+    * @return bool listelenebildiyse doğru, listelenemediyse yanlış değer döndürsün
+    */
+    public function show(){
+        if(!$this->checkLogin()){
+            return false;
+        }
+        $oldData = $this->view();
+        return  array('template'=>'admin','render'=>true,'setting'=>$oldData['setting'], 'renderFile'=>'edit');
+    }
+	
+    /**
+    * Kullanıcıların listelenmesini sağlayan metod. Bu metod boş olmalı
+    *
+    * @return bool listelenebildiyse doğru, listelenemediyse yanlış değer döndürsün
+    */
+    public function index(){
+        if(!$this->checkLogin()){
+            return false;
+        }
+        return  $this->show();
+    }
+
+
+    /**
+    * Kullanıcıyı düzenlemeye yarar. Verilen Id bilginse göre, alınan bilgi ile sistemdeki bilgiyi değiştiren
+    * güncelleyen metod. Bu sayfa aynı zamanda kullanıcının profil sayfası olarak da görünmeli. View metodundan
+    * hazır verileri alıp göstersin.
+    *
+    * @param int $id Kategorinin benzersiz index'i
+    * @param string $username Yönetici kullanıcı adı
+    * @param string $username Yönetici parola
+    * @param string $username Yönetici e-posta
+    * @return bool düzenlendiyse doğru, eklenemediyse yanlış değer döndürsün
+    */
+    public function edit($title=null, $description = null, $copyright = null){
+        if(!$this->checkLogin()){
+            return false;
+        }
+        $oldData = $this->view();
+        $id = $oldData['setting']['id'];
+        if($title!=null)
+        {
+            // Önce veritabanı sorgumuzu hazırlayalım.
+            $query = $this->db->prepare("UPDATE settings SET title=:baslik, description=:aciklama, copyright=:altbilgi WHERE id=:id");
+	
+            $update = $query->execute(array(
+                "id"=>$id,
+                "baslik"=>$title,
+                "aciklama"=>$description,
+                "altbilgi"=>$copyright
+            ));
+		
+            if ( $update ){
+                 return true;
+            }
+            else
+            {
+                return false;
+            }  
+        }
+        else{
+            return  array('template'=>'admin','render'=>true,'setting'=>$oldData['setting']);
+        }
+    }
+
+    /**
+    * Kullanıcı silen metod, verilerin silinmesini sağlar.
+    * Geri dönüşü yoktur.
+    *
+    * @param int $id Kategorinin benzersiz index'i
+    * @return bool silindiyse doğru, eklenemediyse yanlış değer döndürsün
+    */
+    public function delete($id=null){
+        return array('template'=>'admin','render'=>false, 'message'=>'Ayar silinemez!');
+    }
+	
+}
+?>
+
