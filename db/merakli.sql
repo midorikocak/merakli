d835a3cbf35299e58a417c34042b5866df5e20eb
Midori Koçak <mtkocak@mtkocak.net>
2014-09-01 08:30:27 +0300

    database for users and settings, ignored files

--- db/merakli.sql
+++ db/merakli.sql
@@ -1,130 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 26, 2014 at 07:34 PM
--- Server version: 5.6.20
--- PHP Version: 5.4.24
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- -----------------------------------------------------
+-- Schema merakli
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `merakli` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
+USE `merakli` ;
+
+-- -----------------------------------------------------
+-- Table `merakli`.`categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `merakli`.`categories` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_turkish_ci;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- -----------------------------------------------------
+-- Table `merakli`.`files`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `merakli`.`files` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `filename` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_turkish_ci;
 
---
--- Database: `merakli`
---
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `merakli`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `merakli`.`posts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
+  `content` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NULL DEFAULT NULL,
+  `category_id` INT(11) NOT NULL,
+  `created` DATETIME NOT NULL,
+  `updated` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `category_id_idx` (`category_id` ASC),
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `merakli`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_turkish_ci;
 
---
--- Table structure for table `categories`
---
 
-CREATE TABLE IF NOT EXISTS `categories` (
-`id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=6 ;
+-- -----------------------------------------------------
+-- Table `merakli`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `merakli`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
---
--- Dumping data for table `categories`
---
 
-INSERT INTO `categories` (`id`, `title`) VALUES
-(1, 'PHP'),
-(2, 'jQuery Nedir?'),
-(3, 'SQL'),
-(4, 'Diğer');
+-- -----------------------------------------------------
+-- Table `merakli`.`settings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `merakli`.`settings` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL,
+  `copyright` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `files`
---
-
-CREATE TABLE IF NOT EXISTS `files` (
-`id` int(11) NOT NULL,
-  `filename` varchar(255) COLLATE utf8_turkish_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `filename`) VALUES
-(8, '258c5.10606532_851306961546723_5074839846515665460_n.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE IF NOT EXISTS `posts` (
-`id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
-  `content` text COLLATE utf8_turkish_ci,
-  `category_id` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=10 ;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `title`, `content`, `category_id`, `created`, `updated`) VALUES
-(1, 'PHP Nedir?', 'PHP (açılımı PHP: Hypertext Preprocessor) geniş bir kitle tarafından kullanılan, özellikle sanal yöreler üzerinde geliştirme için tasarlanmış HTML içine gömülebilen bir betik dilidir.', 1, '2014-08-18 00:00:00', '2014-08-18 00:00:00'),
-(2, 'jQuery Nedir?', 'Jquery bir javascript kütüphanesidir. Ya da bir javascript frameworküdür diyebiliriz. Daha çok sayfalardaki animasyonlar için Flash alternatifi olarak kullanılan bu teknoloji ile, slaytlar, foto galeriler, tab menüler yapmak oldukça kolaydır.', 2, '2014-08-18 00:00:00', '2014-08-18 00:00:00'),
-(3, 'OOP''ye giriş', 'OOP''yi çok severiz. OOP kullanmamak çok sıkıntı çıkarır, spaghetti insanın midesini bozar.', 1, '2014-08-23 11:46:55', '2014-08-23 13:20:23'),
-(5, 'Nesne Yönelimli Programlama nasıl yapılmalı', 'İnce eleyip sık dokuyarak', 1, '2014-08-26 18:53:07', '2014-08-26 18:53:07');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `files`
---
-ALTER TABLE `files`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `files`
---
-ALTER TABLE `files`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
