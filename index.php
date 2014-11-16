887ddd95fbcf3cd18839632ea4fe9e4a3533dd7d
Yagiz Nizipli <yagiznizipli@me.com>
2014-11-17 00:28:38 +0200

    Add project-wide xss header protection.

--- index.php
+++ index.php
@@ -4,17 +4,14 @@ require_once 'Vendor/BasicDB/basicdb.php';
 require 'Config/config.inc.php';
 require_once 'Vendor/autoload.php';
 
-
-
-
 $directoryName = basename(dirname(__FILE__));
 
 define('LINK_PREFIX', 'http://' . $_SERVER['HTTP_HOST'] . '/' . $directoryName);
 define('FILE_PREFIX', 'http://' . $_SERVER['HTTP_HOST'] . '/' . $directoryName . '/www/');
 define('DIRECTORY_NAME',$directoryName);
+header('X-XSS-Protection: 0');
 
 $app = new \Midori\Cms\App($config);
-
 
 $endTime = microtime(true);
 $elapsed = $endTime - $startTime;
