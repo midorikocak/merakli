53e6b52b2778c9fc1f66acb9dba9bd963b2fc6e8
Midori Koçak <mtkocak@mtkocak.net>
2014-11-16 21:21:51 +0200

    all views reformat

---
+++ View/Files/view.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * One file is shown using this layout
+ * Raw data will be injected here as $file variable and handled by this layout.
+ *
+ * @author Midori Kocak 2014
+ *
+ */
+?>
+
+<article>
+	<h2><?php echo $file['id']; ?></h2>
+
+	<p><?= $file['filename'] ?></p>
+</article>
+
+
---
+++ View/Posts/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Posts are shown in public context. This file serves to present raw data as html tags.
+ *
+ * @author Midori Kocak 2014
+ *
+ */
+if (isset($posts) && ! empty($posts)) :
+    foreach ($posts as $post) :
+        ?>
+<article>
+	<h2><?php echo $post['title']; ?></h2>
+
+	<p><?= substr(strip_tags($post['content']), 0, 40) ?>... <a
+			href="<?= LINK_PREFIX ?>/Posts/View/<?= $post['id'] ?>">Devamını Oku</a>
+	</p>
+	<small><?= $post['created'] ?></small>
+</article>
+<?php
+    endforeach
+    ;
+
+endif;
+?>
+
