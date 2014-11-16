9c886437043c8315cd0db277765f279511260912
Midori Koçak <mtkocak@mtkocak.net>
2014-11-16 16:42:41 +0200

    Translation of app.php

--- App/App.php
+++ App/App.php
@@ -1,8 +1,10 @@
 <?php
 /**
-* Uygulamamızı çalıştıracak olan sınıf
+* The class that starts our application.
 *
-* Sistemdeki tüm sınıfların içermeleri gereken veritabanı ve diğer bilgileri tutan sınıf.
+* Works as dependency injector.
+ * TODO Have to check solid principle violations.
+ * TODO Have to convert the class to static and use registry pattern for instantiated classes
 *
 * @author   Midori Kocak <mtkocak@mtkocak.net>
 */
@@ -21,34 +23,47 @@ class App
 {
 
     /**
-     * Veritabanı bağlantısını tutacak olan değişken.
+     * The variable that holds database connection.
      *
      * @var PDO
      */
     private $db = false;
 
     /**
-     * Sistem ayarlarını çeken değişken.
+     * Holds application wide settings
      *
      * @var mixed
      */
     protected $settings = false;
 
     /**
+     * Holds request params, coming from uri.
+     *
      * @var mixed
      */
     private $params = array();
 
     /**
+     * Extracted params for request generation
+     *
      * @var array
      */
     private $request;
 
     /**
+     * The data that comes from forms or classes
+     *
      * @var mixed
      */
     private $data = null;
 
+
+    /**
+     * Function that starts application using config vars
+     *
+     * @param $config
+     * @return void
+     */
     public function __construct($config)
     {
         $this->startSession();
@@ -97,12 +112,24 @@ class App
         }
     }
 
+    /**
+     * Instantiates database connection
+     * TODO has to be static.
+     *
+     * @param $config
+     * @return void
+     */
     public function startDB($config)
     {
         $db = new erbilen\BasicDB($config['db']['host'], $config['db']['dbname'], $config['db']['username'], $config['db']['password']);
         $this->getDb($db);
     }
 
+    /**
+     * Session starter function for the whole app
+     *
+     * @return void
+     */
     public function startSession()
     {
         $sess_name = session_name();
@@ -111,6 +138,10 @@ class App
         }
     }
 
+    /**
+     * If the application has no user registered, visitor is forced to create an user.
+     *
+     */
     public function startUserInstaller()
     {
         if (! isset($_POST['user'])) {
@@ -130,6 +161,10 @@ class App
         }
     }
 
+    /**
+     * If the application has no settings created, visitor is forced to create the setting.
+     *
+     */
     public function startSettingInstaller()
     {
         if (isset($_POST['setting'])) {
@@ -139,6 +174,12 @@ class App
         }
     }
 
+    /**
+     * If the application has no post or category created, user is forced to create one, after login.
+     *
+     * @param $request
+     * @param $data
+     */
     public function startCategoryAndPostInstaller($request, $data)
     {
         $noCategories = $this->getCategories() == null;
@@ -159,6 +200,11 @@ class App
         }
     }
 
+    /**
+     * Requests are handled by this method
+     *
+     * @return void
+     */
     public function getRequests()
     {
         if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) !== false) {
@@ -177,23 +223,22 @@ class App
             }
         }
         if (! empty($_POST)) {
-            $data = $_POST;
+            $this->data = $_POST;
         } elseif (! empty($_FILES)) {
-            $data = $_FILES;
+            $this->data = $_FILES;
         } else {
-            $data = "";
+            $this->data = "";
         }
         if (empty($request)) {
-            $request = "/Posts/";
+            $this->request = "/Posts/";
         }
         $this->request = $request;
     }
 
     /**
-     * Veritabanına bağlanmaya yarayan kurucu metod
+     * Method starts database connection
      *
      * @param BasicDBObject $dbConnection
-     *            Veritabanı işlemleri sınıfı
      * @return BasicDB object or False.
      */
     public function getDb($dbConnection)
@@ -207,7 +252,7 @@ class App
     }
 
     /**
-     * Nesnelere bağlı olan bilgileri çektiğimiz metod
+     * Categories are shown everywhere. So we need to get them to an array.
      *
      * @return array
      */
@@ -217,12 +262,22 @@ class App
         return $categories->index();
     }
 
+    /**
+     * We need to check posts to redirect user to create some posts if there are not.
+     *
+     * @return array
+     */
     public function getPosts()
     {
         $posts = new Posts($this->db);
         return $posts->index();
     }
 
+    /**
+     * Renders login form if authorized user actions are needed
+     *
+     * @param null $data
+     */
     public function login($data = null)
     {
         $users = new Users($this->db);
@@ -234,9 +289,9 @@ class App
     }
 
     /**
-     * Tüm sitedeki ayarları çektiğimiz metod
+     * Basic settings are also needed everywhere.
      *
-     * @return array
+     * @return bool.
      */
     public function getSettings()
     {
@@ -250,6 +305,11 @@ class App
         }
     }
 
+    /**
+     * Database creation form is handled here.
+     *
+     * @return void
+     */
     public function startDatabaseInstaller()
     {
         if (isset($_POST['db']['host'])) {
@@ -275,6 +335,11 @@ class App
         }
     }
 
+    /**
+     * Form to create setting is rendered and created.
+     *
+     * @param null $settings
+     */
     public function installSettings($settings = null)
     {
         if ($settings == null) {
@@ -294,6 +359,11 @@ class App
         }
     }
 
+    /**
+     * Method to check system has any registered user
+     *
+     * @return bool
+     */
     public function getUsers()
     {
         $user = $this->db->select('users')->run();
@@ -303,6 +373,11 @@ class App
         return true;
     }
 
+    /**
+     * Method for installing database config to config.php
+     *
+     * @param null $config
+     */
     public function installDatabase($config = null)
     {
         $comments = "";
@@ -321,6 +396,11 @@ class App
         }
     }
 
+    /**
+     * Handles user registration form.
+     *
+     * @param null $userInfo
+     */
     public function installUser($userInfo = null)
     {
         if ($userInfo == null) {
@@ -341,19 +421,20 @@ class App
     }
 
     /**
-     * Sistemdeki bütün görüntüleme hesaplama işlemlerini yapan metod
-     *
-     * @param
-     *            $request
-     * @param
-     *            $data
-     * @return string
+     * Renders requests and handles data from visitor.
+     * @return void
      */
     public function calculate()
     {
         echo new Router($this->data, $this->getCategories(), $this->settings, $this->params);
     }
 
+    /**
+     * Renders file and variables
+     *
+     * @param $file
+     * @param $vars
+     */
     public function render($file, $vars)
     {
         $renderer = new Router();
