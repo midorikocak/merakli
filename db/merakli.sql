2c52586cf4c3ff0080262683794768e7bb43b66b
Midori Koçak <mtkocak@mtkocak.net>
2014-11-18 19:45:30 +0200

    new database for auto install

--- db/merakli.sql
+++ db/merakli.sql
@@ -1,106 +1,161 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 4.2.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 18, 2014 at 06:44 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.5.14
 
--- -----------------------------------------------------
--- Schema merakli
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `merakli` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
-USE `merakli` ;
-
--- -----------------------------------------------------
--- Table `merakli`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `merakli`.`categories` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_turkish_ci;
-
-INSERT INTO `categories` (`id`, `title`) VALUES
-(1, 'PHP'),
-(2, 'jQuery Nedir?'),
-(3, 'SQL'),
-(4, 'Diğer');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `merakli`.`files`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `merakli`.`files` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `filename` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_turkish_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `merakli`
+--
 
--- -----------------------------------------------------
--- Table `merakli`.`posts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `merakli`.`posts` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NOT NULL,
-  `content` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_turkish_ci' NULL DEFAULT NULL,
-  `category_id` INT(11) NOT NULL,
-  `created` DATETIME NOT NULL,
-  `updated` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `category_id_idx` (`category_id` ASC),
-  CONSTRAINT `category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `merakli`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_turkish_ci;
+-- --------------------------------------------------------
 
-INSERT INTO `posts` (`id`, `title`, `content`, `category_id`, `created`, `updated`) VALUES
-(1, 'PHP Nedir?', 'PHP (açılımı PHP: Hypertext Preprocessor) geniş bir kitle tarafından kullanılan, özellikle sanal yöreler üzerinde geliştirme için tasarlanmış HTML içine gömülebilen bir betik dilidir.', 1, '2014-08-18 00:00:00', '2014-08-18 00:00:00'),
-(3, 'OOP''ye giriş', 'OOP''yi çok severiz. OOP kullanmamak çok sıkıntı çıkarır, spaghetti insanın midesini bozar.', 1, '2014-08-23 11:46:55', '2014-08-23 13:20:23'),
-(5, 'Nesne Yönelimli Programlama nasıl yapılmalı', 'İnce eleyip sık dokuyarak', 1, '2014-08-26 18:53:07', '2014-08-26 18:53:07'),
-(6, 'jQuery Nedir?', 'Jquery', 2, '2014-09-21 17:17:42', '2014-09-21 17:17:42');
+--
+-- Table structure for table `categories`
+--
 
+CREATE TABLE IF NOT EXISTS `categories` (
+`id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `merakli`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `merakli`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `files`
+--
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'username', 'mtkocak@mtkocak.net', '5f4dcc3b5aa765d61d8327deb882cf99');
+CREATE TABLE IF NOT EXISTS `files` (
+`id` int(11) NOT NULL,
+  `filename` varchar(255) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `posts`
+--
 
--- -----------------------------------------------------
--- Table `merakli`.`settings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `merakli`.`settings` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL,
-  `description` TEXT NULL,
-  `copyright` TEXT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `posts` (
+`id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `content` text COLLATE utf8_turkish_ci,
+  `category_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=1 ;
 
-INSERT INTO `settings` (`id`, `title`, `description`, `copyright`) VALUES
-(1, 'Merakli Bilişimci', 'Merakli İnternet Mühendisinden meraklılara geliyor', 'Copyright 2014 Meraklibilismci.com');
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `settings`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE IF NOT EXISTS `settings` (
+`id` int(11) NOT NULL,
+  `title` varchar(45) COLLATE utf8_turkish_ci NOT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `copyright` text COLLATE utf8_turkish_ci
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+ ADD PRIMARY KEY (`id`), ADD KEY `category_id_idx` (`category_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
